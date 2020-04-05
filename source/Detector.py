47618d01df805d8c6ad40ce867e8a22cf22f29d6
a-alex-l <lesnik-a-a@yandex.ru>
2020-04-05 17:48:21 +0300

    Detector circles fixed, now count of circles can be more 1. Line detection returned to Canny for fixing rectangles of lines.

--- source/Detector.py
+++ source/Detector.py
@@ -1,6 +1,6 @@
 import cv2
 import math
-import numpy
+import numpy as np
 import random
 
 
@@ -47,32 +47,34 @@ class Circle:
 
 
 def detector(  # input_image
-        block_size=101,
-        level_black=10,
-        rho=1,
-        threshold=100,
-        min_line_length=100,
-        max_line_gap=10,
-        min_dist_between_centers=10,
-        threshold_canny=10,
-        threshold_center=10,
-        min_radius=0,
-        max_radius=0):
-    input_image = cv2.imread("/home/alex/Pictures/MapLapFile3.png")
-    gray_image = make_contrast_and_gray_image(input_image, block_size, level_black)
-    lines = detect_lines_without_width(gray_image, rho, threshold, min_line_length, max_line_gap)
-    circles = detect_circles_without_width(gray_image, min_dist_between_centers,
-                                           threshold_canny, threshold_center, min_radius, max_radius)
+        block_size: int = 101,
+        level_black: int = 20,
+
+        threshold: int = 100,
+        min_line_length: int = 15,
+        max_line_gap: int = 10,
+
+        threshold_canny: int = 100,
+        threshold_center: int = 100,
+        min_radius: float = 0,
+        max_radius: float = 0) -> list:
+    input_image = cv2.imread("/home/alex/Pictures/MapLapFile8.png")
+    gray_image = get_gray_image(input_image, block_size, level_black)
+    circles_centers = detect_centers_of_circles(gray_image, threshold_canny, threshold_center, min_radius, max_radius)
+    circles = find_radiuses_and_line_width(gray_image, circles_centers)
+    canny_image = get_canny_edge_image(gray_image, threshold_canny)
+    lines = detect_lines_without_width(canny_image, threshold, min_line_length, max_line_gap)
     print(lines.__len__())
     print("\n".join(map(str, lines)))
     print(circles.__len__())
     print("\n".join(map(str, circles)))
     show_finds(input_image, lines, circles)
+    return lines + circles
 
 
-def make_contrast_and_gray_image(const_image: cv2.UMat,
-                                 block_size=101,
-                                 level_black=10):
+def get_gray_image(const_image: cv2.UMat,
+                   block_size: int = 101,
+                   level_black: int = 20) -> np.ndarray:
     gray_image = cv2.cvtColor(const_image, cv2.COLOR_BGR2GRAY)
     gray_image = cv2.adaptiveThreshold(gray_image,
                                        maxValue=255,
@@ -80,17 +82,24 @@ def make_contrast_and_gray_image(const_image: cv2.UMat,
                                        thresholdType=cv2.THRESH_BINARY_INV,
                                        blockSize=block_size,  # Parameter!
                                        C=level_black)  # Parameter!
-    cv2.imshow("Show Canny", gray_image)
+    cv2.imshow("Show Gaussian", gray_image)
     return gray_image
 
 
-def detect_lines_without_width(const_image: numpy.ndarray,
-                               rho=1,
-                               threshold=100,
-                               min_line_length=100,
-                               max_line_gap=10) -> list:
+def get_canny_edge_image(const_image: np.ndarray, threshold_canny: int = 200) -> np.ndarray:
+    canny_image = cv2.Canny(const_image,
+                            threshold1=threshold_canny,  # 100% edge
+                            threshold2=threshold_canny / 2)  # if connected to edge its edge too.
+    cv2.imshow("Show Canny", canny_image)
+    return canny_image
+
+
+def detect_lines_without_width(const_image: np.ndarray,
+                               threshold: int = 100,
+                               min_line_length: int = 15,
+                               max_line_gap: int = 10) -> list:
     coordinates: list = cv2.HoughLinesP(const_image,
-                                        rho=rho,  # Parameter!
+                                        rho=1,
                                         theta=math.pi / 180,
                                         threshold=threshold,  # threshold for line detection
                                         minLineLength=min_line_length,  # minimal line length
@@ -103,34 +112,40 @@ def detect_lines_without_width(const_image: numpy.ndarray,
     return lines
 
 
-def detect_circles_without_width(const_image: numpy.ndarray,
-                                 min_dist_between_centers=10,
-                                 threshold_canny=10,
-                                 threshold_center=10,
-                                 min_radius=0,
-                                 max_radius=0) -> list:
-    centers_ans_radiuses: list = cv2.HoughCircles(const_image,
-                                                  cv2.HOUGH_GRADIENT,
-                                                  1,  #
-                                                  min_dist_between_centers,  # minimal distance between two centers
-                                                  param1=threshold_canny,  # threshold for Canny edge detector
-                                                  param2=threshold_center,  # threshold for center detection
-                                                  minRadius=min_radius,
-                                                  maxRadius=max_radius)
+def detect_centers_of_circles(const_image: np.ndarray,
+                              threshold_canny: int = 200,
+                              threshold_center: int = 100,
+                              min_radius: float = 0,
+                              max_radius: float = 0) -> list:
+    centers: list = cv2.HoughCircles(const_image,
+                                     cv2.HOUGH_GRADIENT,
+                                     1,
+                                     minDist=10,
+                                     param1=threshold_canny,  # threshold for Canny edge detector
+                                     param2=threshold_center,  # threshold for center detection
+                                     minRadius=min_radius,
+                                     maxRadius=max_radius)
+    if centers is not None:
+        return centers[0, :]
+    else:
+        return list()
+
+
+def find_radiuses_and_line_width(canny_image: np.ndarray, centers) -> list:
     circles = []
-    if centers_ans_radiuses is not None:
-        for center_ans_radius in centers_ans_radiuses:
-            x_center, y_center, radius = center_ans_radius[0]
-            circles.append(Circle(x_center, y_center, radius, 1))
+    if centers is not None:
+        for center in centers:
+            x_center, y_center, radius = center
+            circles.append(Circle(x_center, y_center, radius, 0))
     return circles
 
 
 def show_finds(const_image: cv2.UMat, lines: list, circles: list):
     image = cv2.UMat(const_image).get()
     if lines:
-            for line in lines:
-                cv2.line(image, (line.x_first, line.y_first), (line.x_second, line.y_second),
-                         (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        for line in lines:
+            cv2.line(image, (line.x_first, line.y_first), (line.x_second, line.y_second),
+                     (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
     if circles:
         for circle in circles:
             cv2.circle(image, (circle.x_center, circle.y_center), circle.radius,
@@ -139,4 +154,4 @@ def show_finds(const_image: cv2.UMat, lines: list, circles: list):
     cv2.waitKey()
 
 
-detector(level_black=20)
+detector()
