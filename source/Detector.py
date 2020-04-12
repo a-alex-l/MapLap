ea8071207d69f825c1192099220bb833c0f20a45
ASUS <Krisha11>
2020-04-12 23:14:02 +0300

    + add generator images

--- source/Detector.py
+++ source/Detector.py
@@ -1,3 +1,42 @@
-import cv2
+class Line:
+    x_first: int = 0
+    y_first: int = 0
+    x_second: int = 0
+    y_second: int = 0
+    line_width: float = 0
 
-print("Create new branch for writing Detector.")
+    def __init__(self, x_first: int, y_first: int, x_second: int, y_second: int, line_width: int = 1):
+        self.x_first = x_first
+        self.y_first = y_first
+        self.x_second = x_second
+        self.y_second = y_second
+        self.line_width = line_width
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.x_first.__str__() + " " + self.y_first.__str__() + " " \
+               + self.x_second.__str__() + " " + self.y_second.__str__() + " " + self.line_width.__str__()
+
+
+class Circle:
+    x_center: float = 0
+    y_center: float = 0
+    radius: float = 0
+    line_width: float = 0
+
+    def __init__(self, x_center: int, y_center: int, radius: int, line_width: int = 1):
+        self.x_center = x_center
+        self.y_center = y_center
+        self.radius = radius
+        self.line_width = line_width
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.x_center.__str__() + " " + self.y_center.__str__() + " " \
+               + self.radius.__str__() + " " + self.line_width.__str__()
+
+# detector branch
--- source/GeneratorImages.py
+++ source/GeneratorImages.py
@@ -1,2 +1,28 @@
+import random
+from source.Detector import *
 
-print("there will be generator images")
+
+def generator_images(random_seed=0, circles=5, lines=5, thickness="thin", width=100, height=100) -> list:
+    # ->ListOfLinesAndCircles
+    random.seed(random_seed)
+    figures = []
+    line_width = 1
+    if thickness == "thick":
+        line_width = 5
+
+    for i in range(circles):
+        x_center = random.randint(1, width - 1)
+        y_center = random.randint(1, height - 1)
+        radius = random.randint(1, min(x_center, width - x_center, y_center, height - y_center))
+        figures.append(Circle(x_center, y_center, radius, line_width))
+
+    for i in range(lines):
+        x_first = random.randint(1, width)
+        y_first = random.randint(1, height)
+        x_second = random.randint(1, width)
+        y_second = random.randint(1, height)
+        figures.append(Line(x_first, y_first, x_second, y_second, line_width))
+    return figures
+
+
+generator_images()
