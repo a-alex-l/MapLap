df569d0751b574c351af704b00890d6a6e566fe1
Андрей <p-a-k-12@mail.ru>
2020-04-18 01:21:40 +0000

    + painting, work with pictures, general improvements, developments, constatnt.py

--- maplap/Interface.py
+++ maplap/Interface.py
@@ -1,24 +1,16 @@
 import sys
-from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtWidgets, QtGui, QtCore, Qt
 import design
+import constant as C
 
-#to const file?
-START_PICTURE = "maplap/templates/MapLap.png"
-SIZE_PANEL = 140
-SIZE_LINE = 25
-BORDER = 9
-ROTATE = 90
-FULL_ROTATE = 360
-MIN_H = 300
-ZERO = 0
-DOUBLE = 2
-INIT_FACTOR = 1.0
-UPDATE = True
-TEMPLATE_FILE = "*.png *.pdf *.jpg *.bmp"
-#
+class Rectangle:
+    def __init__(self, x1 = C.ZERO, y1 = C.ZERO, x2 = C.ZERO, y2 = C.ZERO):
+        self.x_start = x1
+        self.y_start = y1
+        self.x_end = x2
+        self.y_end = y2
 
-width_screen = ZERO; height_screen = ZERO
-
+width_screen = C.ZERO; height_screen = C.ZERO
 
 class MainWindow(QtWidgets.QMainWindow, design.Ui_MapLap):
     def __init__(self):
@@ -30,70 +22,177 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MapLap):
         self.save_tex.clicked.connect(self.__save_tex)
         self.save_pdf.clicked.connect(self.__save_pdf)
         self.rotate.clicked.connect(self.__rotate)
+        self.pencil.clicked.connect(self.__pencil)
+        self.eraser.clicked.connect(self.__eraser)
 
-        self.angle = ZERO
-        self.picture = START_PICTURE
-        self.picture_factor = INIT_FACTOR
-
-        pixmap = QtGui.QPixmap(START_PICTURE)
-        self.picture_in.setPixmap(pixmap)
-        self.picture_out.setPixmap(pixmap)
-        self.picture_in._pixmap = QtGui.QPixmap(self.picture_in.pixmap())
-        self.picture_out._pixmap = QtGui.QPixmap(self.picture_in.pixmap())
-        self.resize(DOUBLE * (SIZE_LINE + pixmap.width()) + SIZE_PANEL, pixmap.height())
-
-    def update_image(self):
-        pixmap = QtGui.QPixmap(self.picture)
-        pixmap = pixmap.transformed(QtGui.QTransform().rotate(self.angle))
-        new_width = DOUBLE * (SIZE_LINE + pixmap.width()) + SIZE_PANEL
-        #handle maximum screen size!
-        self.setMinimumSize(QtCore.QSize(new_width / pixmap.height() * MIN_H, MIN_H))
-        self.picture_in.setPixmap(pixmap)
-        self.picture_out.setPixmap(pixmap)
-        self.picture_in._pixmap = QtGui.QPixmap(self.picture_in.pixmap())
-        self.picture_out._pixmap = QtGui.QPixmap(self.picture_out.pixmap())
-        self.resize(new_width * self.picture_factor, pixmap.height() * self.picture_factor)
-        self.resizeEvent(UPDATE)
-
-    def __cropping(self):
-        pass
-
-    def __select_area(self):
-        pass
-
-    def __choose_file(self):
-        dialog = QtWidgets.QFileDialog(self)
-        dialog.setFileMode(QtWidgets.QFileDialog.ExistingFile)
-        dialog.setNameFilter(TEMPLATE_FILE)
-        if dialog.exec_():
-            self.picture = "".join(dialog.selectedFiles())
-            self.angle = ZERO
-            self.picture_factor = INIT_FACTOR
-            self.update_image()
+        self.angle = [C.ZERO, C.ZERO]
+        self.picture = self.picture_res = C.START_PICTURE
+        self.picture_factor = C.INIT_FACTOR
+        self.action = "no"
+        self.area = Rectangle()
+        self.need_update = False
+        self.is_area_up = False
+        self.picture_in.mouseMoveEvent = self.do_nothing
+        self.picture_in.mousePressEvent = self.do_nothing
+        self.picture_in.mouseReleaseEvent = self.do_nothing
+        self.path = Qt.QPainterPath()
+        self.init_picture()
 
     def __save_tex(self):
         #changing, save ...
         pass
         
     def __save_pdf(self):
-        #save
-        pass
+        self.picture_in.pixmap().save("maplap/templates/temp.pdf", "PDF")
+        ###
+
+    def __cropping(self):
+        self.picture_res = C.RES
+        self.save_image(C.PICTURE_OUT)
+        self.update_image()
+        ##
+
+    def init_picture(self):
+        self.resize_window()
+        self.picture = C.TEMP
+        self.save_image()
+        self.__cropping()
+        self.resize_window()
+
+    def save_image(self, is_picture_in = True, format = C.FORMAT):
+        if is_picture_in:
+            self.picture_in.pixmap().save(C.TEMP, format)
+            self.angle[0] = C.ZERO
+        else:
+            self.picture_in.pixmap().save(C.RES, format)
+            self.angle[1] = C.ZERO
+
+    def resize_window(self):
+        pixmap = QtGui.QPixmap(self.picture)
+        pixmap_res = QtGui.QPixmap(self.picture_res)
+        pixmap = pixmap.transformed(QtGui.QTransform().rotate(self.angle[0]))
+        pixmap_res = pixmap_res.transformed(QtGui.QTransform().rotate(self.angle[1]))
+        width_some = C.DOUBLE * C.SIZE_LINE + C.SIZE_PANEL
+        new_width = min(pixmap.width() + pixmap_res.width() + width_some, width_screen)
+        self.setMinimumSize(QtCore.QSize(new_width / pixmap.height() * C.MIN_H, C.MIN_H))
+        self.resize((pixmap.width() + pixmap_res.width()) * self.picture_factor + C.SIZE_PANEL, pixmap.height() * self.picture_factor)
+        self.update_image()
+
+    def update_image(self):
+        width = self.width()
+        height = self.height()
+        pixmap = QtGui.QPixmap(self.picture)
+        pixmap_res = QtGui.QPixmap(self.picture_res)
+        pixmap = pixmap.transformed(QtGui.QTransform().rotate(self.angle[0]))
+        pixmap_res = pixmap_res.transformed(QtGui.QTransform().rotate(self.angle[1]))
+        self.picture_in.setPixmap(pixmap)
+        self.picture_out.setPixmap(pixmap_res)
+        self.picture_in._pixmap = QtGui.QPixmap(self.picture_in.pixmap())
+        self.picture_out._pixmap = QtGui.QPixmap(self.picture_out.pixmap())
+        self.resize(width, height)
+        self.resizeEvent(C.UPDATE)
+
+    def __select_area(self):
+        if self.action == "select":
+            self.change_action("no")
+        else:
+            self.change_action("select")
+        
+    def change_action(self, mode):
+        if mode == "no":
+            self.select_area.setText("select area")
+            self.picture_in.setProperty("cursor", QtGui.QCursor(QtCore.Qt.ArrowCursor))
+            self.picture_in.mouseMoveEvent = self.do_nothing
+            self.picture_in.mousePressEvent = self.do_nothing
+            self.picture_in.mouseReleaseEvent = self.do_nothing
+            self.resizeEvent(C.UPDATE)
+        elif mode == "select":
+            self.select_area.setText("no select area")
+            self.picture_in.setProperty("cursor", QtGui.QCursor(QtCore.Qt.CrossCursor))
+            self.picture_in.mouseMoveEvent = self.mouseMoveEvent_select
+            self.picture_in.mousePressEvent = self.mousePressEvent_select
+            self.picture_in.mouseReleaseEvent = self.mouseReleaseEvent_select
+        elif mode == "eraser":
+            self.select_area.setText("select area")
+            self.picture_in.setProperty("cursor", QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+            self.picture_in.mouseMoveEvent = self.draw
+            self.picture_in.mousePressEvent = self.set_point
+            self.picture_in.mouseReleaseEvent = self.end_path
+            self.resizeEvent(C.UPDATE)
+        elif mode == "pencil":
+            self.select_area.setText("select area")
+            self.picture_in.setProperty("cursor", QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+            self.picture_in.mouseMoveEvent = self.draw
+            self.picture_in.mousePressEvent = self.set_point
+            self.picture_in.mouseReleaseEvent = self.end_path
+            self.resizeEvent(C.UPDATE)
+        else:
+            print("error in change_action")
+            self.change_action("no")
+        self.action = mode
+
+    def __choose_file(self):
+        dialog = QtWidgets.QFileDialog(self)
+        dialog.setFileMode(QtWidgets.QFileDialog.ExistingFile)
+        dialog.setNameFilter(C.TEMPLATE_FILE)
+        if dialog.exec_():
+            self.picture = "".join(dialog.selectedFiles())
+            self.angle[0] = C.ZERO
+            self.picture_factor = C.INIT_FACTOR
+            self.init_picture()
 
     def __rotate(self):
         pixmap = QtGui.QPixmap(self.picture)
-        pixmap = pixmap.transformed(QtGui.QTransform().rotate(self.angle))
-        self.picture_factor = (self.picture_in.height() + DOUBLE * BORDER) / pixmap.height()
-        self.angle = (self.angle + ROTATE) % FULL_ROTATE
-        self.update_image()
+        pixmap = pixmap.transformed(QtGui.QTransform().rotate(self.angle[0]))
+        self.picture_factor = (self.picture_in.height() + C.DOUBLE * C.BORDER) / pixmap.height()
+        self.angle[0] = (self.angle[0] + C.ROTATE) % C.FULL_ROTATE
+        self.angle[1] = (self.angle[1] + C.ROTATE) % C.FULL_ROTATE
+        self.resize_window()
+
+    def check_area_coord(self):
+        if self.area.x_start > self.area.x_end:
+            self.area.x_start, self.area.x_end = self.area.x_end, self.area.x_start
+        if self.area.y_start > self.area.y_end:
+            self.area.y_start, self.area.y_end = self.area.y_end, self.area.y_start
+        self.area.x_start = max(self.area.x_start, C.ZERO)
+        self.area.y_start = max(self.area.y_start, C.ZERO)
+        self.area.x_end = min(self.area.x_end, self.picture_in.width())
+        self.area.y_end = min(self.area.y_end, self.picture_in.height() + C.CURSOR)
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Return: #enter
-            print('enter')
-            #...
-        elif event.key() == QtCore.Qt.Key_Escape:
-            print('exit')
-            #...
-
+            if self.is_area_up:
+                self.resizeEvent(C.UPDATE)
+                self.check_area_coord()
+                self.copy = self.picture_in.pixmap().copy(self.area.x_start, self.area.y_start, 
+                    self.area.x_end - self.area.x_start, self.area.y_end - self.area.y_start)
+                self.picture_in.setPixmap(self.copy)
+                self.save_image()
+                self.resize_window()
+        if event.key() == QtCore.Qt.Key_Backspace:
+            if self.is_area_up:
+                self.resizeEvent(C.UPDATE)
+                self.check_area_coord()
+                self.erase_rect()
+                self.save_image()
+                self.resize_window()
+        elif event.key() == QtCore.Qt.Key_F5:
+            self.__cropping()
+        elif event.key() == QtCore.Qt.Key_F4:
+            self.__rotate()
+        elif event.modifiers():
+            if event.key() == QtCore.Qt.Key_Up:
+                self.move(self.pos().x() - C.STEP, self.pos().y())
+            if event.key() == QtCore.Qt.Key_Down:
+                self.move(self.pos().x() + C.STEP, self.pos().y())
+            elif event.key() == QtCore.Qt.Key_Escape:
+                exit(0)
+        else:
+            if event.key() == QtCore.Qt.Key_Up:
+                self.move(self.pos().x(), self.pos().y() - C.STEP)
+            if event.key() == QtCore.Qt.Key_Down:
+                self.move(self.pos().x(), self.pos().y() + C.STEP)
+            
     def resizeEvent(self, event):
         self.picture_in.setPixmap(self.picture_in._pixmap.scaled(
             self.width(), self.height(),
@@ -101,11 +200,81 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MapLap):
         self.picture_out.setPixmap(self.picture_out._pixmap.scaled(
             self.width(), self.height(),
             QtCore.Qt.KeepAspectRatio))
-
-    def focusInEvent(self, event):
+        self.is_area_up = False
+        
+    def do_nothing(self, event):
         pass
 
-    #...
+    def mousePressEvent_select(self, event):
+        if self.action == "select":
+            self.resizeEvent(C.UPDATE)
+            self.area.x_start = event.x()
+            self.area.y_start = event.y() + C.CURSOR
+        
+    def mouseMoveEvent_select(self, event):
+        if self.action == "select":
+            self.need_update = True
+            self.area.x_end = event.x()
+            self.area.y_end = event.y() + C.CURSOR
+            self.picture_in.update()
+
+    def mouseReleaseEvent_select(self, event):
+        if self.action == "select":
+            self.area.x_end = event.x()
+            self.area.y_end = event.y() + C.CURSOR
+
+    def paintEvent(self, event):
+        if self.action == "select" and self.need_update:
+            self.need_update = False
+            self.update_image()
+            self.paint = QtGui.QPainter()
+            self.paint.begin(self.picture_in.pixmap())
+            self.paint.setOpacity(C.OPACITY_AREA)
+            self.paint.setBrush(QtGui.QBrush(QtCore.Qt.blue))
+            self.paint.drawRect(self.area.x_start, self.area.y_start,
+                self.area.x_end - self.area.x_start, self.area.y_end - self.area.y_start)
+            self.paint.end()
+            self.is_area_up = True
+        
+    def __pencil(self):
+        if self.action == "pencil":
+            self.change_action("no")
+        else:
+            self.change_action("pencil")
+        
+    def __eraser(self):
+        if self.action == "eraser":
+            self.change_action("no")
+        else:
+            self.change_action("eraser")
+
+    def set_point(self, event):
+        self.path.moveTo(event.pos())
+
+    def draw(self, event):
+        self.path.lineTo(event.pos())
+        draw = Qt.QPainter(self.picture_in.pixmap())
+        draw.setPen(self.set_pen())
+        draw.drawPath(self.path)
+        self.picture_in.update()
+
+    def end_path(self, event):
+        self.save_image()
+        self.path = Qt.QPainterPath()
+        self.update_image()
+
+    def erase_rect(self):
+        draw = Qt.QPainter(self.picture_in.pixmap())
+        draw.setPen(self.set_pen(C.DELETE))
+        draw.setBrush(QtCore.Qt.white)
+        draw.drawRect(self.area.x_start, self.area.y_start,
+                self.area.x_end - self.area.x_start, self.area.y_end - self.area.y_start)
+
+    def set_pen(self, delete = False):
+        if self.action == "pencil" and not delete:
+            return Qt.QPen(QtCore.Qt.black, C.BLACK_W)
+        else:
+            return Qt.QPen(QtCore.Qt.white, C.WHITE_W)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
@@ -115,7 +284,6 @@ def main():
     window = MainWindow()
     window.show()
     app.exec_()
-    
 
 if __name__ == '__main__':
     main()
---
+++ maplap/constant.py
@@ -0,0 +1,27 @@
+##interface
+
+START_PICTURE = "maplap/templates/MapLap.png"
+TEMP = "maplap/templates/temp.png"
+RES = "maplap/templates/MapLap_res.png"
+FORMAT = "PNG"
+SIZE_PANEL = 140
+SIZE_LINE = 25
+BORDER = 9
+ROTATE = 90
+FULL_ROTATE = 360
+MIN_H = 300
+ZERO = 0
+DOUBLE = 2
+INIT_FACTOR = 1.0
+CURSOR = 8.0
+STEP = 10
+BLACK_W = 1
+WHITE_W = 5
+OPACITY_AREA = 0.3
+UPDATE = True
+TEMPLATE_FILE = "*.png *.pdf *.jpg *.bmp"
+CHANGE = True
+PICTURE_OUT = False
+DELETE = True
+
+##
