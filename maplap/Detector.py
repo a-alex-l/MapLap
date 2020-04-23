38c0798a52cb4bd4fc7438be055e84d9fffa42d7
a-alex-l <lesnik-a-a@yandex.ru>
2020-04-23 21:21:49 +0300

    Decrease asymptotics of clarifing from O(found_circles * radius_each_circle^2 * image_height * image_width) to O(Circles_on_picture * radius_each_circle * max_thickness).

--- maplap/Detector.py
+++ maplap/Detector.py
@@ -11,7 +11,12 @@ class Line:
     y_second: int = 0
     line_width: int = 0
 
-    def __init__(self, x_first: int, y_first: int, x_second: int, y_second: int, line_width: int = 1):
+    def __init__(self,
+                 x_first: int,
+                 y_first: int,
+                 x_second: int,
+                 y_second: int,
+                 line_width: int = 1):
         self.x_first = x_first
         self.y_first = y_first
         self.x_second = x_second
@@ -49,22 +54,25 @@ def detector(input_image: cv2.UMat,
              level_black: int = 20,
 
              threshold_line: int = 100,
-             min_line_length: int = 15,
-             max_line_gap: int = 10,
+             min_line_length: int = 5,
+             max_line_gap: int = 5,
+
+             is_circle: float = 0.6,
+             max_thickness: int = 20,
 
              find_rate: int = 1,
-             threshold_center: int = 12,
-             min_radius: int = 0,
+             threshold_center: int = 35,
+             min_radius: int = 5,
              max_radius: int = 0) -> list:
     gray_image = get_black_white_image(input_image, block_size, level_black)
     canny_image = cv2.Canny(gray_image, 100, 50)
-    cv2.imshow("Show Canny", canny_image)
-
-    circles_centers = detect_centers_of_circles(gray_image, find_rate, threshold_center, min_radius, max_radius)
     lines = detect_lines_without_width(canny_image, threshold_line, min_line_length, max_line_gap)
-
-    circles = find_radius_and_line_width(gray_image, circles_centers)
-    show_finds(input_image, lines, circles)
+    circles_centers = detect_centers_of_circles(gray_image,
+                                                find_rate,
+                                                threshold_center,
+                                                min_radius,
+                                                max_radius)
+    circles = clarify_circles(gray_image, circles_centers, is_circle, max_thickness, min_radius)
     return lines + circles
 
 
@@ -78,7 +86,6 @@ def get_black_white_image(const_image: cv2.UMat,
                                        thresholdType=cv2.THRESH_BINARY_INV,
                                        blockSize=block_size,  # Parameter!
                                        C=level_black)  # Parameter!
-    cv2.imshow("Show Gaussian", gray_image)
     return gray_image
 
 
@@ -100,12 +107,27 @@ def detect_lines_without_width(const_image: np.ndarray,
     return lines
 
 
-def detect_centers_of_circles(const_image: np.ndarray,
-                              find_rate: int = 2,
-                              threshold_center: int = 100,
+def count_intersections(gray_image: np.ndarray,
+                        x_center: int,
+                        y_center: int,
+                        radius: int) -> int:
+    count: int = 0
+    if x_center - radius < 0 or x_center + radius >= gray_image.shape[1]\
+            or y_center - radius < 0 or y_center + radius >= gray_image.shape[0]:
+        return 0
+    for phi in np.arange(0.0, 2 * np.pi, 5 / radius):
+        if gray_image[round(y_center + radius * math.sin(phi))]\
+           [round(x_center + radius * math.cos(phi))] != 0:
+            count = count + 5
+    return count
+
+
+def detect_centers_of_circles(gray_image: np.ndarray,
+                              find_rate: int = 1,
+                              threshold_center: int = 20,
                               min_radius: int = 0,
                               max_radius: int = 0) -> list:
-    centers: list = cv2.HoughCircles(const_image,
+    centers: list = cv2.HoughCircles(gray_image,
                                      cv2.HOUGH_GRADIENT,
                                      find_rate,
                                      minDist=0.0001,
@@ -114,55 +136,98 @@ def detect_centers_of_circles(const_image: np.ndarray,
                                      minRadius=min_radius,
                                      maxRadius=max_radius)
     if centers is not None:
-        return centers[0, :]
+        circles: list = []
+        if centers is not None:
+            for center in centers[0, :]:
+                x_center, y_center, radius = map(int, center)
+                circles.append(Circle(x_center, y_center, radius, 1))
+        return circles
     else:
         return list()
 
 
-def is_here_circle(gray_image: np.ndarray,
-                   x_center: int,
-                   y_center: int,
-                   radius: int,
-                   is_circle: float = 0.7) -> bool:
-    circle_image = np.zeros(gray_image.shape, gray_image.dtype)
-    cv2.circle(circle_image, (x_center, y_center), radius, (255, 255, 255))
-    and_image = cv2.bitwise_and(gray_image, circle_image, mask=None)
-    cv2.imshow("Circle", circle_image)
-    cv2.imshow("And", and_image)
-    #cv2.waitKey()
-    return cv2.countNonZero(and_image) > is_circle * cv2.countNonZero(circle_image)
+def find_radius_and_line_width(gray_image: np.ndarray, circle: Circle, is_circle: float) -> Circle:
+    circle.line_width = 1
+    while count_intersections(gray_image,
+                              circle.x_center,
+                              circle.y_center,
+                              circle.radius + circle.line_width) \
+            > is_circle * 2 * np.pi * (circle.radius + circle.line_width):
+        circle.line_width = circle.line_width + 1
+    circle.line_width = circle.line_width - 1
+    return circle
 
 
-def find_radius_and_line_width(gray_image: np.ndarray, centers) -> list:
-    circles = []
-    if centers is not None:
-        for center in centers:
-            x_center, y_center, radius = map(int, center)
-            if is_here_circle(gray_image, x_center, y_center, radius):
-                line_width = 1
-                while is_here_circle(gray_image, x_center, y_center, radius + line_width):
-                    line_width = line_width + 1
-                line_width = line_width - 1
-                if line_width != 0:
-                    circles.append(Circle(x_center, y_center, radius, line_width))
+def find_right_center_and_radius(gray_image: np.ndarray,
+                                 circle: Circle,
+                                 is_circle: float = 0.6,
+                                 max_thickness: int = 20,
+                                 min_radius: int = 5) -> Circle:
+    count_non_zero_now = \
+        count_intersections(gray_image, circle.x_center, circle.y_center, circle.radius)
+    moves = ((1, 0), (0, 1), (-1, 0), (0, -1),
+             (1, 0), (0, 1), (-1, 0), (0, -1),
+             (1, 0), (0, 1), (-1, 0), (0, -1))
+    for move in moves:
+        for _ in range(1, max_thickness):
+            count_non_zero_move = count_intersections(gray_image,
+                                                      circle.x_center + move[0],
+                                                      circle.y_center + move[1],
+                                                      circle.radius)
+            if count_non_zero_move > is_circle * 2 * np.pi * circle.radius:
+                circle.x_center = circle.x_center + move[0]
+                circle.y_center = circle.y_center + move[1]
+                count_non_zero_now = count_non_zero_move
+            else:
+                break
+            if count_non_zero_now > is_circle * 2 * np.pi * circle.radius and\
+               circle.radius > min_radius:
+                count_non_zero_new = count_intersections(gray_image,
+                                                         circle.x_center,
+                                                         circle.y_center,
+                                                         circle.radius - 1)
+                if count_non_zero_new > is_circle * 2 * np.pi * (circle.radius - 1):
+                    circle.radius = circle.radius - 1
+                    count_non_zero_now = count_non_zero_new
+    return circle
+
+
+def clarify_center(gray_image: np.ndarray, circle: Circle, is_circle: float = 0.6,
+                   min_radius: int = 5) -> Circle:
+    circle = find_right_center_and_radius(gray_image, circle, is_circle, min_radius)
+    circle = find_radius_and_line_width(gray_image, circle, is_circle)
+    return circle
+
+
+def find_shadow_circles(gray_image: np.ndarray, centers: list, is_circle: float = 0.6) -> list:
+    return list()
+
+
+def find_concetric_circle(gray_image: np.ndarray, centers: list, is_circle: float = 0.6) -> list:
+    return list()
+
+
+def clarify_circles(gray_image: np.ndarray,
+                    centers: list,
+                    is_circle: float = 0.6,
+                    max_thickness: int = 20,
+                    min_radius: int = 5) -> list:
+    circles: list = []
+    for center in centers:
+        if count_intersections(gray_image, center.x_center, center.y_center, center.radius) \
+                > is_circle * 2 * np.pi * center.radius:
+            center = clarify_center(gray_image, center, is_circle, min_radius)
+            for center_delete in centers:
+                if math.hypot(center.x_center - center_delete.x_center,
+                              center.y_center - center_delete.y_center) <= \
+                        2 * center.line_width:
+                    centers.remove(center_delete)
+            for center_delete in circles:
+                if math.hypot(center.x_center - center_delete.x_center,
+                              center.y_center - center_delete.y_center) <= \
+                        2 * center.line_width:
+                    circles.remove(center_delete)
+            circles.append(center)
+    circles += find_shadow_circles(gray_image, centers, is_circle)
+    circles += find_concetric_circle(gray_image, circles, is_circle)
     return circles
-
-
-def show_finds(const_image: cv2.UMat, lines: list, circles: list):
-    image = cv2.UMat(const_image).get()
-    print("Lines ", len(lines))
-    print("Circles ", len(circles))
-    if lines:
-        for line in lines:
-            cv2.line(image, (line.x_first, line.y_first), (line.x_second, line.y_second),
-                     (random.randint(0, 255), random.randint(0, 255), 255), line.line_width)
-    if circles:
-        for circle in circles:
-            cv2.circle(image, (circle.x_center, circle.y_center), circle.radius,
-                       (255, random.randint(0, 255), random.randint(0, 255)), circle.line_width)
-    cv2.imshow("Show", image)
-    cv2.waitKey()
-
-
-input_image = cv2.imread("/home/alex/Pictures/MapLapFile10.png")
-detector(input_image)
