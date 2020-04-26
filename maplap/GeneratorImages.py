3ec9e9fb916290b001113c56c14f126c8a344842
ASUS <Krisha11>
2020-04-26 12:29:48 +0300

    style changes

--- maplap/GeneratorImages.py
+++ maplap/GeneratorImages.py
@@ -1,24 +1,26 @@
 import random
-from maplap.Detector import *
+from detector import Circle, Line, Point
 
 
-def generator_images(random_seed=0, circles=5, lines=5, max_thickness=1, width=100, height=100) -> list:
+def generator_images(random_seed=0, circles=5, lines=5,
+                     max_thickness=1, size=100) -> list:
     # ->ListOfLinesAndCircles
     random.seed(random_seed)
     figures = []
 
-    for i in range(circles):
-        x_center = random.randint(1, width - 1)
-        y_center = random.randint(1, height - 1)
-        radius = random.randint(1, min(x_center, width - x_center, y_center, height - y_center))
+    for _ in range(circles):
+        x_center = random.randint(1, size)
+        y_center = random.randint(1, size)
+        radius = random.randint(1, min(x_center, size - x_center, y_center, size - y_center))
         line_width = random.randint(1, max_thickness)
-        figures.append(Circle(x_center, y_center, radius, line_width))
+        figures.append(Circle(Point(x_center, y_center), radius, line_width))
 
-    for i in range(lines):
-        x_first = random.randint(1, width)
-        y_first = random.randint(1, height)
-        x_second = random.randint(1, width)
-        y_second = random.randint(1, height)
+    for _ in range(lines):
+        x_first = random.randint(1, size)
+        y_first = random.randint(1, size)
+        x_second = random.randint(1, size)
+        y_second = random.randint(1, size)
         line_width = random.randint(1, max_thickness)
-        figures.append(Line(x_first, y_first, x_second, y_second, line_width))
+        figures.append(Line(Point(x_first, y_first), Point(x_second, y_second), line_width))
+
     return figures
--- maplap/detector.py
+++ maplap/detector.py
@@ -1,43 +1,47 @@
-class Line:
-    x_first: int = 0
-    y_first: int = 0
-    x_second: int = 0
-    y_second: int = 0
-    line_width: float = 0
+class Point:
+    x: int
+    y: int
 
-    def __init__(self, x_first: int, y_first: int, x_second: 
-                 int, y_second: int, line_width: int = 1):
-        self.x_first = x_first
-        self.y_first = y_first
-        self.x_second = x_second
-        self.y_second = y_second
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.x} {self.y}"
+
+
+class Line:
+    point_first: Point
+    point_second: Point
+    line_width: int
+
+    def __init__(self, point_first: Point, point_second: Point, line_width: int):
+        self.point_first = point_first
+        self.point_second = point_second
         self.line_width = line_width
 
     def __repr__(self):
-        return self.__str__()
+        return str(self)
 
     def __str__(self):
-        return self.x_first.__str__() + " " + self.y_first.__str__() + " " \
-               + self.x_second.__str__() + " " + self.y_second.__str__() + " " + self.line_width.__str__()
+        return f"{self.point_first} {self.point_second} {self.line_width}"
 
 
 class Circle:
-    x_center: float = 0
-    y_center: float = 0
-    radius: float = 0
-    line_width: float = 0
+    center: Point
+    radius: int
+    line_width: int
 
-    def __init__(self, x_center: int, y_center: int, radius: int, line_width: int = 1):
-        self.x_center = x_center
-        self.y_center = y_center
+    def __init__(self, center: Point, radius: int, line_width: int = 1):
+        self.center = center
         self.radius = radius
         self.line_width = line_width
 
     def __repr__(self):
-        return self.__str__()
+        return str(self)
 
     def __str__(self):
-        return self.x_center.__str__() + " " + self.y_center.__str__() + " " \
-               + self.radius.__str__() + " " + self.line_width.__str__()
-
-# from detector branch
+        return f"{self.center} {self.radius} {self.line_width}"
