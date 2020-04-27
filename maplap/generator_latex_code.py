1b35a9979ca0b9aa9d30d5dca238daf281b96620
ASUS <Krisha11>
2020-04-27 21:54:39 +0300

    change pdf format

--- maplap/generator_latex_code.py
+++ maplap/generator_latex_code.py
@@ -3,21 +3,39 @@ from pylatex import Document, TikZ, TikZCoordinate, TikZDraw, TikZOptions
 from detector import Circle, Line
 
 
-# TikZNode, TikZUserPath
 def generator_latex_code(list_of_lines_and_circles) -> Document:
-    doc = Document()
+    geometry_options = {
+        "margin": "0in",
+        # "paperheight": str(height) + "pt",
+        # "paperwidth": str(width) + "pt",
+    }
 
+    # some scaling problems
+
+    doc = Document(page_numbers=False, geometry_options=geometry_options)
     with doc.create(TikZ()) as pic:
         for i in list_of_lines_and_circles:
             if isinstance(i, Circle):
-                node_kwargs = {"line width": str(i.line_width) + "pt", "radius": str(i.radius)}
+                node_kwargs = {
+                    "line width": str(i.line_width) + "pt",
+                    "radius": str(i.radius),
+                    "scale": "0.15",
+                }
                 center = TikZCoordinate(i.center.x_coordinate, i.center.y_coordinate)
-                pic.append(TikZDraw([center, "circle"], options=TikZOptions(**node_kwargs)))
+                pic.append(
+                    TikZDraw([center, "circle"], options=TikZOptions(**node_kwargs))
+                )
             elif isinstance(i, Line):
-                node_kwargs = {"line width": str(i.line_width) + "pt"}
-                first = TikZCoordinate(i.point_first.x_coordinate, i.point_first.y_coordinate)
-                second = TikZCoordinate(i.point_second.x_coordinate, i.point_second.y_coordinate)
-                pic.append(TikZDraw([first, "--", second], options=TikZOptions(**node_kwargs)))
+                node_kwargs = {"line width": str(i.line_width) + "pt", "scale": "0.15"}
+                first = TikZCoordinate(
+                    i.point_first.x_coordinate, i.point_first.y_coordinate
+                )
+                second = TikZCoordinate(
+                    i.point_second.x_coordinate, i.point_second.y_coordinate
+                )
+                pic.append(
+                    TikZDraw([first, "--", second], options=TikZOptions(**node_kwargs))
+                )
 
     return doc
 
@@ -32,5 +50,7 @@ def generator_latex_tex(list_of_lines_and_circles, filename):
 
 
 def generator_latex_pdf(list_of_lines_and_circles, filename):
+    # some problems with saving existed file
+
     doc = generator_latex_code(list_of_lines_and_circles)
     doc.generate_pdf(filename)
