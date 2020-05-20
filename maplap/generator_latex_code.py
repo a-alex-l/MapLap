18a1ce08301a71afdbc40b677b05e35f30900970
ASUS <Krisha11>
2020-05-20 20:45:08 +0300

    add scale

--- maplap/generator_latex_code.py
+++ maplap/generator_latex_code.py
@@ -1,11 +1,8 @@
-from pylatex import Document, UnsafeCommand, TikZ, TikZCoordinate, TikZDraw, TikZOptions, NoEscape
-from pylatex.base_classes import CommandBase, Arguments, Command
-from pylatex.package import Package
+from pylatex import Document, TikZ, TikZOptions, NoEscape
 
-from geometry import Circle, Line
+import tkinter as tk
 
-
-# delete useless \usepackage
+from geometry import Circle, Line, Point
 
 
 class GeneratorLatexCode:
@@ -13,66 +10,39 @@ class GeneratorLatexCode:
     def __init__(self, list_of_lines_and_circles):
         geometry_options = {
             "margin": "0in",
-            # "textwidth" : "1pt"
             # "paperheight": str(height) + "pt",
             # "paperwidth": str(width) + "pt",
         }
+        self.list_of_lines_and_circles = self.reverse_y(list_of_lines_and_circles)
         self.doc = Document(page_numbers=False, geometry_options=geometry_options)
-        # circle_comm = UnsafeCommand('newcommand', r'\Circle', options=5,
-        #                             extra_arguments=r'\draw[line width = #1, scale = #3] (#4, #5) circle (#2)')
-        # self.doc.append(circle_comm)
-
-        # line_comm = UnsafeCommand('newcommand', r'\Line', options=6,
-        #                           extra_arguments=r'\draw[line width = #1, scale = #2] (#3, #4) -- (#5, #6)')
-        # self.doc.append(line_comm)
-
-        # some scaling problems
-        self.pic = self.generator_pic(list_of_lines_and_circles)
+        self.pic = self.generator_pic()
         self.doc.append(self.pic)
 
-    # class CircleCommand(CommandBase):
-    #     # extra_arguments=r'\draw[line width = #1, radius = #2, scale = #3] (#4, #5) circle')
-    #     _latex_name = 'Circle'
-    #     packages = [Package('tikz')]
-    #
-    #     def dumps(self):
-    #         options = self.options.dumps()
-    #         arguments = self.arguments.dumps()
-    #
-    #         if self.extra_arguments is None:
-    #             return r'\{command}{options}{arguments};' \
-    #                 .format(command=self.latex_name, options=options,
-    #                         arguments=arguments)
-    #
-    #         extra_arguments = self.extra_arguments.dumps()
-    #
-    #         return r'\{command}{arguments}{options}{extra_arguments};' \
-    #             .format(command=self.latex_name, arguments=arguments,
-    #                     options=options, extra_arguments=extra_arguments)
-    #
-    # class LineCommand(CommandBase):
-    #     # extra_arguments=r'\draw[line width = #1, scale = #2] (#3, #4) -- (#5, #6)'
-    #     _latex_name = 'Line'
-    #     packages = [Package('tikz')]
-    #
-    #     def dumps(self):
-    #         options = self.options.dumps()
-    #         arguments = self.arguments.dumps()
-    #
-    #         if self.extra_arguments is None:
-    #             return r'\{command}{options}{arguments};' \
-    #                 .format(command=self.latex_name, options=options,
-    #                         arguments=arguments)
-    #
-    #         extra_arguments = self.extra_arguments.dumps()
-    #
-    #         return r'\{command}{arguments}{options}{extra_arguments};' \
-    #             .format(command=self.latex_name, arguments=arguments,
-    #                     options=options, extra_arguments=extra_arguments)
+    @staticmethod
+    def reverse_y(list_of_lines_and_circles):
+        max_y_coord = 0
+        for i in list_of_lines_and_circles:
+            if isinstance(i, Circle):
+                max_y_coord = max(max_y_coord, i.center.y_coord)
+            elif isinstance(i, Line):
+                max_y_coord = max(max_y_coord, i.start.y_coord, i.end.y_coord)
+        new_list = []
+        for i in list_of_lines_and_circles:
+            if isinstance(i, Circle):
+                new_list.append(Circle(Point(i.center.x_coord, max_y_coord - i.center.y_coord), i.radius, i.line_width))
+            elif isinstance(i, Line):
+                new_list.append(Line(Point(i.start.x_coord, max_y_coord - i.start.y_coord),
+                                     Point(i.end.x_coord, max_y_coord - i.end.y_coord), i.line_width))
+        return new_list
 
     @staticmethod
     def __get_scale() -> float:
-        return 0.03
+        root = tk.Tk()
+        width_px = root.winfo_screenwidth()
+        width_mm = root.winfo_screenmmwidth()
+        width_cm = width_mm / 10
+        width_cmpd = width_cm / width_px
+        return width_cmpd
 
     @staticmethod
     def __circle_command_string(width, x, y, radius):
@@ -82,17 +52,6 @@ class GeneratorLatexCode:
         picture.append(NoEscape(self.__circle_command_string(circle.line_width,
                                                              circle.center.x_coord, circle.center.y_coord,
                                                              circle.radius)))
-        # picture.append(self.CircleCommand(arguments=Arguments(circle.line_width, circle.radius, self.get_scale(),
-        #                                                       circle.center.x_coord, circle.center.y_coord)))
-        # node_kwargs = {
-        #     "line width": str(circle.line_width),
-        #     "radius": str(circle.radius),
-        #     "scale": str(get_scale()),
-        # }
-        # center = TikZCoordinate(circle.center.x_coord, circle.center.y_coord)
-        # pic.append(
-        #     TikZDraw([center, "circle"], options=TikZOptions(**node_kwargs))
-        # )
 
     @staticmethod
     def __line_command_string(width, x1, y1, x2, y2):
@@ -102,18 +61,11 @@ class GeneratorLatexCode:
         picture.append(NoEscape(self.__line_command_string(line.line_width,
                                                            line.start.x_coord, line.start.y_coord,
                                                            line.end.x_coord, line.end.y_coord)))
-        # picture.append(self.LineCommand(arguments=Arguments(line.line_width, self.get_scale(),
-        #                                                      line.start.x_coord, line.start.y_coord,
-        #                                                      line.end.x_coord, line.end.y_coord)))
-        # node_kwargs = {"line width": str(line.line_width), "scale": str(self.get_scale())}
-        # first = TikZCoordinate(line.start.x_coord, line.start.y_coord)
-        # second = TikZCoordinate(line.end.x_coord, line.end.y_coord)
-        # picture.append(TikZDraw([first, "--", second], options=TikZOptions(**node_kwargs)))
 
-    def generator_pic(self, list_of_lines_and_circles) -> TikZ:
-        options = {"scale": str(self.__get_scale())}
+    def generator_pic(self) -> TikZ:
+        options = {"scale": f'{self.__get_scale():.4f}'}
         picture = TikZ(options=TikZOptions(**options))
-        for i in list_of_lines_and_circles:
+        for i in self.list_of_lines_and_circles:
             if isinstance(i, Circle):
                 self.__add_circle(picture, i)
             elif isinstance(i, Line):
@@ -124,8 +76,6 @@ class GeneratorLatexCode:
         return self.doc.dumps()
 
     def generator_one_picture_packages_str(self) -> str:
-        # no ideas how it works
-
         return self.pic.dumps_packages()
 
     def generator_one_picture_str(self) -> str:
@@ -135,6 +85,4 @@ class GeneratorLatexCode:
         self.doc.generate_tex(filename)
 
     def generator_latex_pdf(self, filename):
-        # some problems with saving existed file
-
         self.doc.generate_pdf(filename)
